c82390e279f922163f3633ce23bb2b1af3b43cb0
Mistty <shenhaichen@outlook.com>
2017-11-19 01:38:19 +1100

    little revise.

--- syd/src/syd/Main.java
+++ syd/src/syd/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello, dou.");
 		System.out.println("Hello, dou.");
+		System.out.print("SH in SH, with SH.");
 	}
 }
